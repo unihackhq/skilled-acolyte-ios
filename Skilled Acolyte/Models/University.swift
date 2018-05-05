e361cce003d645891c4b9301584c35b3f02fb7d6
Daniel Sykes-Turner <dst@localz.co>
2018-05-05 17:37:28 +1000

    Added university

---
+++ Skilled Acolyte/Models/University.swift
@@ -0,0 +1,22 @@
+//
+//  University.swift
+//  Skilled Acolyte
+//
+//  Created by Daniel Sykes-Turner on 5/5/18.
+//  Copyright © 2018 UNIHACK Inc. All rights reserved.
+//
+
+import Foundation
+
+class University: NSObject {
+    
+    var id: String!
+    var name: String!
+    var country: String
+    
+    init(data: [String:Any]) {
+        id = data["id"] as! String
+        name = data["name"] as! String
+        country = data["country"] as! String
+    }
+}
