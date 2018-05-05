a9ec34598937a9192f9307dd7f8fb709f5336584
Daniel Sykes-Turner <dst@localz.co>
2018-05-05 17:52:45 +1000

    Added ticket

---
+++ Skilled Acolyte/Models/Ticket.swift
@@ -0,0 +1,26 @@
+//
+//  Ticket.swift
+//  Skilled Acolyte
+//
+//  Created by Daniel Sykes-Turner on 5/5/18.
+//  Copyright © 2018 UNIHACK Inc. All rights reserved.
+//
+
+import Foundation
+
+class Ticket: NSObject {
+    
+    var id: String!
+    var eventbriteOrder: String!
+    var ticketType: String!
+    var transferred: Bool! = false
+    var cancelled: Bool! = false
+    
+    init(data: [String:Any]) {
+        id = data["id"] as! String
+        eventbriteOrder = data["eventbriteOrder"] as! String
+        ticketType = data["ticketType"] as! String
+        if let transferred = data["transferred"] as? Bool { self.transferred = transferred }
+        if let cancelled = data["cancelled"] as? Bool { self.cancelled = cancelled }
+    }
+}
