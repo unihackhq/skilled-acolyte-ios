08e0221ac7005ecff487db74ed52a30c21fe31a4
Daniel Sykes-Turner <dst@localz.co>
2018-05-20 17:53:24 +1000

    Fixed networking file (obj-c to swift)

--- Skilled Acolyte/Networking.swift
+++ Skilled Acolyte/Networking.swift
@@ -2,51 +2,56 @@
 //  Networking.swift
 //  Skilled Acolyte
 //
-//  Created by Daniel Sykes-Turner on 5/5/18.
+//  Created by Daniel Sykes-Turner on 20/5/18.
 //  Copyright © 2018 UNIHACK Inc. All rights reserved.
 //
 
 import Foundation
 
-class Networking {
+class Networking: NSObject {
     
-    var sessionManager: URLSession!
+    let session: URLSession!
     
-    init() {
-//        let url = URL(string: Constants.host)!
-//        let config = URLSessionConfiguration.default
-//        let request = NSMutableURLRequest(url: url)
-//        request.httpMethod = "POST"
-//        let body = "something"
-//        request.httpBody = body.data(using: String.Encoding.utf8)
-//        sessionManager = URLSession(configuration: config)
+    override init() {
+        // We must initlize non-nil variables before super.init()
+        self.session = URLSession(configuration: URLSessionConfiguration.default)
         
-//        open func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Swift.Void) -> URLSessionDataTask
-
-//        URLSession.shared.dataTask(with: request, completionHandler: { (responseData: Data?, response: URLResponse?, error: Error?) in
-//            NSLog("\(response)")
-//        }).resume()
-        
-//        self.sessionManager.dataTask(with: request) { (data, response, error) -> Void in
-//            if let data = data {
-//                if let response = response as? HTTPURLResponse, 200...299 ~= response.statusCode {
-//                    success(data , response , error as? NSError)
-//                } else {
-//                    failure(data , response as? HTTPURLResponse, error as? NSError)
-//                }
-//            } else {
-//                failure(data , response as? HTTPURLResponse, error as? NSError)
-//            }
-//        }.resume()
-        
-//        self.sessionManager.dataTask(with: request, completionHandler: { (data, response, error) in
-//
-//        }
-//        let task = sessionManager.dataTask(with: request, completionHandler: { (data:Data?, response:URLResponse?, error:Error?) in
-//
-//        })
+        super.init()
     }
     
-    
-//    func request(httpMethod: String, url: String, body: [String:Any], completionHandler)
+    func request(with httpMethod:String!, url:String!, body:[String:Any]?, completion:((Error?, [String:Any]?) -> Void)?) {
+        
+        // Build a http request
+        let request = NSMutableURLRequest(url: URL(string: url)!)
+        request.httpMethod = httpMethod
+        // Encode the body as json (if it exists)
+        if let body = body {
+            do {
+                let jsonEncode = try JSONSerialization.data(withJSONObject: body, options: .prettyPrinted)
+                request.httpBody = jsonEncode
+            } catch {
+                print("Failed to encode json: \(error.localizedDescription)")
+            }
+        }
+
+        // Make the request (.resume() to start the request)
+        session.dataTask(with: request as URLRequest) { (data, response, error) in
+
+            // Unpack data response if it exisits
+            var jsonDecode:[String:Any]?
+            if let data = data {
+                do {
+                    jsonDecode = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.init(rawValue: 0)) as? [String : Any]
+                } catch {
+                    print("Failed to decode json: \(error.localizedDescription)")
+                }
+            }
+            
+            // Call completion if it exists
+            if let completion = completion {
+                completion(error, jsonDecode)
+            }
+            
+        }.resume()
+    }
 }
